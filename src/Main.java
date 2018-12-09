8b3f86d4d870c9abfbb93c12dfb4d34629200f7b
Дмитрий <k.dima098@yandex.ru>
2018-12-09 16:19:36 +0300

    Fully done inserting with all balancing.

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,10 @@ public class Main {
 
     }
 
+    private int height (Node current) {
+        return current == null ? -1 : current.height;
+    }
+
     private void insert (int key) {
         root = insert(key, root);
     }
@@ -31,53 +35,78 @@ public class Main {
     private Node insert (int key, Node current) {
         if (current == null) {
             current = new Node(key, null);
-            return current;
         }
-        else if (key < root.key) {
+        else if (key < current.key) {
             current.leftChild = insert(key, current.leftChild);
             current = balance(current, key);
         }
-        else {
+        else if (key > current.key){
             current.rightChild = insert(key, current.rightChild);
             current = balance(current, key);
         }
+        current.height = Math.max(height(current.leftChild), height(current.rightChild)) + 1;
         return current;
     }
 
     private Node balance (Node current, int key) {
-        if (current.leftChild.height - current.rightChild.height == 2) {
+        if (height(current.leftChild) - height(current.rightChild) == 2) {
             if (key < current.leftChild.key) {
-                // TODO Small left rotate
+                current = smallLeftRotate(current);
             }
             else {
-                // TODO Big left rotate
+                current = bigLeftRotate(current);
             }
         }
-        else if (current.rightChild.height - current.leftChild.height == 2) {
+        else if (height(current.rightChild) - height(current.leftChild) == 2) {
             if (key > current.rightChild.key) {
-                // TODO Small right rotate
+                current = smallRightRotate(current);
             }
             else {
-                // TODO Big right rotate
+                current = bigRightRotate(current);
             }
         }
+
         return current;
     }
 
-    private Node setHeight (Node current) {
-        // TODO reheight
-        return current;
+    private Node smallLeftRotate (Node current) {
+        Node currLeftChild = current.leftChild;
+        current.leftChild = currLeftChild.rightChild;
+        currLeftChild.rightChild = current;
+        current.height = Math.max(height(current.leftChild), height(current.rightChild)) + 1;
+        currLeftChild.height = Math.max(height(currLeftChild.leftChild), current.height) + 1;
+        return currLeftChild;
+    }
+
+    private Node smallRightRotate (Node current) {
+        Node currRightChild = current.rightChild;
+        current.rightChild = currRightChild.leftChild;
+        currRightChild.leftChild = current;
+        current.height = Math.max(height(current.leftChild), height(current.rightChild)) + 1;
+        currRightChild.height = Math.max(height(currRightChild.rightChild), current.height) + 1;
+        return currRightChild;
+    }
+
+    private Node bigLeftRotate (Node current) {
+        current.leftChild = smallRightRotate(current.leftChild);
+        return smallLeftRotate(current);
+    }
+
+    private Node bigRightRotate (Node current) {
+        current.rightChild = smallLeftRotate(current.rightChild);
+        return smallRightRotate(current);
     }
 
     public static void main(String[] args) {
         Main tree = new Main();
 
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(0);
-        tree.insert(-1);
-        tree.insert(5);
-        tree.insert(10);
+        tree.insert(15);
+        tree.insert(14);
+        tree.insert(25);
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(7);
+        tree.insert(6);
 
     }
 }
